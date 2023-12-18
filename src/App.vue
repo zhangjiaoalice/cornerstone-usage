91d277ea77a232b0a69e8a206a3ce2c54b11abbf
zhangjiaoalice <2640847812@qq.com>
2023-12-18 19:42:32 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ImageViewport/>
+  </div>
+</template>
+
+<script>
+import ImageViewport from './components/image-viewport.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ImageViewport
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/image-viewport.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div id="image-id">医学影像成像平台</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageViewport',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
